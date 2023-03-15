764d39f332af242a2ae4eade021a84c1218c1e44
Othman <othman.assas@cpexterne.org>
2023-03-15 13:19:19 +0100

    show books

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import useBookSearch from "./useBookSearch";
 function App() {
   const [query, setQuery] = useState("");
   const [pageNumber, setPageNumber] = useState("");
-  useBookSearch(query, pageNumber);
+  const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
   function handleChange(e) {
     setQuery(e.target.value);
     setPageNumber(1);
   }
-
   return (
     <>
       <input type="text" onChange={handleChange} />
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Loading...</div>
-      <div>Error</div>
+      {error && <div>Error</div>}
+      {books.map((b) => <div key={b}>{b}</div>)}
+      {hasMore && loading && <div>Loading...</div>}
     </>
   );
 }
